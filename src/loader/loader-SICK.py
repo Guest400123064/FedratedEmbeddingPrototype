6cf3acfb19463a6aff402a5a9443dab427b80e8c
Guest400123064 <lgfz1003@icloud.com>
2020-12-09 20:17:22 +0800

    .

---
+++ src/loader/loader-SICK.py
@@ -0,0 +1,9 @@
+# import sys
+# sys.path.insert(1, "../helper")
+
+import torch
+import numpy as np
+import pandas as pd
+
+from src.helper.WordEmbedding import WordEmbedding
+
--- src/sim-txt/normal/loader-SICK.py
+++
@@ -1,6 +0,0 @@
-import torch
-import numpy as np
-import pandas as pd
-
-from src.cls.WordEmbedding import WordEmbedding
-
